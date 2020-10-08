4e9dddd5240be8cb531396bcdcf73eb63824a878
AlfredoZum <alfredo.conomia@gmail.com>
2020-10-07 23:19:12 -0500

    Updates

---
+++ precache-manifest.7f64efc5a2128890e5b8455053686220.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2dd14b86a648196023056b317a4a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c4d5c3107b79c01106",
+    "url": "/static/css/main.deaac4d1.chunk.css"
+  },
+  {
+    "revision": "442f4002caf8dcff2327",
+    "url": "/static/js/2.56d3c5eb.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.56d3c5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c4d5c3107b79c01106",
+    "url": "/static/js/main.aa725614.chunk.js"
+  },
+  {
+    "revision": "2e71319991222b421a36",
+    "url": "/static/js/runtime-main.22e28c68.js"
+  },
+  {
+    "revision": "a0e138922c3476d86f6e893d89c38af1",
+    "url": "/static/media/banner_forma_agua.a0e13892.jpg"
+  },
+  {
+    "revision": "703d3e198b9138256d88142ad8a2492f",
+    "url": "/static/media/banner_info.703d3e19.jpg"
+  },
+  {
+    "revision": "8ae0d24744840a6934150acae0e3e2c6",
+    "url": "/static/media/banner_whiplash-color.8ae0d247.jpg"
+  }
+]);
